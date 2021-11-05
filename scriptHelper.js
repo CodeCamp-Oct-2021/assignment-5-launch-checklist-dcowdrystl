79e45d05dee8e7107aa5fb7b53e8042b960375e5
Dan Cowdry <medtc411@gmail.com>
2021-11-04 19:48:25 -0700

    fixed fuel level and cargo level if condition

--- scriptHelper.js
+++ scriptHelper.js
@@ -53,21 +53,24 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
     if (validateInput(pilot) === "Empty" || validateInput(copilot) === "Empty" || validateInput(fuelLevel) === "Empty" || validateInput(cargoLevel) === "Empty"){
         alert("All fields are required!");
        // event.preventDefault();
+        //faultyItems.style.visibility = "hidden";
     }
     else if (validateInput(pilot) === "Is a Number" || validateInput(copilot)=== "Is a Number"){
         
         alert("Make sure to enter valid information for each field!");
         //event.preventDefault();
+       // faultyItems.style.visibility = "hidden";
     }
     else if ((validateInput(fuelLevel)=== "Not a Number") || (validateInput(cargoLevel)=== "Not a Number")){
         alert("Make sure to enter valid information for each field!");
         //event.preventDefault();
+        //faultyItems.style.visibility = "hidden";
     }
      else {
         pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch`;
         copilotStatus.innerHTML = `Co-pilot ${copilot} is ready for launch`;
      }
-        if (fuelLevel <= 10000) {
+        if (fuelLevel < 10000) {
            faultyItems.style.visibility = "visible";
            launchStatus.innerHTML = "Shuttle Not Ready for Launch";
            launchStatus.style.color = "rgb(199, 37, 78)";
@@ -75,7 +78,7 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
         } else {
            fuelStatus.innerHTML = "Fuel level high enough for launch";
         }
-        if (cargoLevel >= 10000) {
+        if (cargoLevel > 10000) {
            faultyItems.style.visibility = "visible";
            launchStatus.innerHTML = "Shuttle Not Ready for Launch";
            launchStatus.style.color = "rgb(199, 37, 78)";
