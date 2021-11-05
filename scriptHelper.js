03f1d9560107f704f043710b2c00081f2bb382d5
Dan Cowdry <medtc411@gmail.com>
2021-11-05 14:53:05 -0700

    changed else ifs to ifs

--- scriptHelper.js
+++ scriptHelper.js
@@ -60,7 +60,7 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
            fuelStatus.innerHTML = "Fuel level too low for launch";
            cargoStatus.innerHTML = "Cargo mass too heavy for launch";
         } 
-     else if (fuelLevel < 10000 && cargoLevel <= 10000) {
+      if (fuelLevel < 10000 && cargoLevel <= 10000) {
            list.style.visibility = "visible";
            launchStatus.innerHTML = "Shuttle Not Ready for Launch";
            launchStatus.style.color = "rgb(199, 37, 78)";
@@ -69,7 +69,7 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
            fuelStatus.innerHTML = "Fuel level too low for launch";
            cargoStatus.innerHTML = "Cargo mass low enough for launch";
         } 
-     else if (fuelLevel >= 10000 && cargoLevel > 10000) {
+      if (fuelLevel >= 10000 && cargoLevel > 10000) {
             list.style.visibility = "visible";
             launchStatus.innerHTML = "Shuttle Not Ready for Launch";
             launchStatus.style.color = "rgb(199, 37, 78)";
@@ -78,7 +78,7 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
             fuelStatus.innerHTML = "Fuel level high enough for launch";
             cargoStatus.innerHTML = "Cargo mass too heavy for launch";
          } 
-      else if (cargoLevel <= 10000 && fuelLevel >= 10000) {
+       if (cargoLevel <= 10000 && fuelLevel >= 10000) {
            list.style.visibility = "visible";
            launchStatus.innerHTML = "Shuttle is Ready for Launch";
            launchStatus.style.color = "rgb(65, 159, 106)";
